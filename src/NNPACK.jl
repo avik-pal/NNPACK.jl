5888c3b039df3278732131a38c721659f1e1958f
Avik Pal <avikpal@iitk.ac.in>
2018-09-17 11:57:28 +0530

    Make it easier to change the number of threads

--- src/NNPACK.jl
+++ src/NNPACK.jl
@@ -2,8 +2,6 @@ module NNPACK
 
 using Libdl, Requires
 
-const NNPACK_CPU_THREADS = parse(UInt64, ENV["JULIA_NUM_THREADS"])
-
 include("libnnpack_types.jl")
 include("error.jl")
 include("libnnpack.jl")
@@ -19,6 +17,7 @@ include(depsjl_path)
 function __init__()
     check_deps()
     nnp_initialize()
+    global NNPACK_CPU_THREADS = parse(UInt64, ENV["JULIA_NUM_THREADS"])
     @require NNlib="872c559c-99b0-510c-b3b7-b6c96a88d5cd" global has_nnlib=true
     if has_nnlib
         include(joinpath(dirname(@__FILE__), "nnlib.jl"))
